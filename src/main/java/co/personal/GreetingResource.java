9b7bc216cf5915af2fa8c20fb9efea595de1121b
m.moghiny <koosha_pooyeshgar@outlook.com>
2025-02-26 17:55:25 +0330

    dev services

--- src/main/java/co/personal/GreetingResource.java
+++ src/main/java/co/personal/GreetingResource.java
@@ -3,14 +3,39 @@ package co.personal;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.security.Principal;
+import java.util.Objects;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name = "greetings")
+    String greeting;
+
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return greeting;
     }
+
+
+
+
+    @GET
+    @Path("/whoami")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String whoAmI(@Context SecurityContext securityContext){
+        Principal userPrincipal = securityContext.getUserPrincipal();
+        if (Objects.nonNull(userPrincipal))
+            return userPrincipal.getName();
+        else
+            return "ananymous";
+    }
+
 }
